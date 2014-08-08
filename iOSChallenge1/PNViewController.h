5a49d9e929edf82e0cd8610446ad2aa640414bf0
Phil Nichols <philipdnichols@gmail.com>
2014-08-08 01:06:26 -0400

    initial commit

---
+++ iOSChallenge1/PNViewController.h
@@ -0,0 +1,13 @@
+//
+//  PNViewController.h
+//  iOSChallenge1
+//
+//  Created by Philip Nichols on 8/8/14.
+//  Copyright (c) 2014 Phil Nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PNViewController : UIViewController
+
+@end
---
+++ iOSChallenge1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSChallenge1
+//
+//  Created by Philip Nichols on 8/8/14.
+//  Copyright (c) 2014 Phil Nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PNAppDelegate class]));
+    }
+}
